048faebafbf2da2452811def7a56e7a131aa3b4c
Karl-Martin Minkner <kalle@kandru.de>
2017-05-14 18:15:48 +0200

    First commit for tcp proxy

---
+++ proxy/tcp.py
@@ -0,0 +1,74 @@
+import time
+import socket
+import threading
+
+class tcp():
+	def __init__(self, relayPort, remoteAddr, remotePort):
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind(("0.0.0.0", relayPort))
+		self.socket.listen()
+		self.remoteAddr = remoteAddr
+		self.remotePort = remotePort
+		self.clientList = dict()
+		t = threading.Thread(target = self.getFromServer)
+		t.start()
+
+	def getFromServer(self):
+		while True:
+			# send data from remote server to specific client
+			for key in list(self.clientList):
+				try:
+					data = self.clientList[key]['rsocket'].recv(1024)
+					self.clientList[key]['lastseen'] = time.time()
+					self.clientList[key]['csocket'].send(data)
+				except socket.error as msg:
+					pass
+			time.sleep(.001)
+
+	def getFromClient(self, conn, addr):
+		while True:
+			try:
+				# if we see the client the first time
+				if not addr in self.clientList:
+					print('connected: ' + str(addr))
+					rsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					rsocket.connect((self.remoteAddr, self.remotePort))
+					rsocket.setblocking(False)
+					self.clientList[addr] = {
+						'rsocket': rsocket,
+						'csocket': conn,
+						'lastseen': time.time()
+					}
+				data = conn.recv(1024)
+				if len(data) != 0:
+					self.clientList[addr]['rsocket'].send(data)
+				else:
+					self.clientList[addr]['rsocket'].close()
+					del self.clientList[addr]
+					print('disconnected: ' + str(addr))
+					break
+			except:
+				try:
+					self.clientList[addr]['rsocket'].close()
+				except:
+					pass
+				try:
+					self.clientList[addr]['csocket'].close()
+				except:
+					pass
+				del self.clientList[addr]
+				print('disconnected: ' + str(addr))
+				break
+			time.sleep(.001)
+
+	def relay(self):
+		while True:
+			# get data from an connected client
+			conn, addr = self.socket.accept()
+			try:
+				t = threading.Thread(target = self.getFromClient, args=(conn, addr))
+				t.start()
+			except:
+				pass
+
--- ts3proxy.py
+++ ts3proxy.py
@@ -4,11 +4,18 @@
 import threading
 
 from proxy.udp import udp
+from proxy.tcp import tcp
 
 if __name__ == '__main__':
     try:
-        u = udp(9987, '89.163.157.103', 9987)
-        t = threading.Thread(target = u.relay)
-        t.start()
+        ts3Udp = udp(9987, '89.163.157.103', 9987)
+        ts3Filetransfer = tcp(30033, '89.163.157.103', 30033)
+        ts3Serverquery = tcp(10011, '89.163.157.103', 10011)
+        t1 = threading.Thread(target = ts3Udp.relay)
+        t1.start()
+        t2 = threading.Thread(target = ts3Filetransfer.relay)
+        t2.start()
+        t3 = threading.Thread(target = ts3Serverquery.relay)
+        t3.start()
     except KeyboardInterrupt:
         exit(0)

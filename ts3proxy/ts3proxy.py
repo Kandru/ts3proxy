807c79eaa12334c61750158f86239838334d4ef4
Bluscream <Bluscream@users.noreply.github.com>
2017-05-30 01:36:29 +0200

    Update ts3proxy.py

--- ts3proxy/ts3proxy.py
+++ ts3proxy/ts3proxy.py
@@ -22,10 +22,10 @@ def main():
           UDP.append( int(config['ts3server']['relayPort']) )
           UDP.append( config['ts3server']['remoteAddress'] )
           UDP.append( int(config['ts3server']['remotePort']) )
-          ts3Udp = Udp(UDP[0],UDP[1],UDP[2])
+          ts3Udp = Udp(*UDP[:3])
           t1 = threading.Thread(target=ts3Udp.relay)
           t1.start()
-          print("Voice: %s:%s <-> %s:%s"%(UDP[0],UDP[1],UDP[2],UDP[3]))
+          print("Voice: %s:%s <-> %s:%s" % UDP)
 
         if config['ts3FileTransfer']['enabled']:
           FT = []
@@ -33,10 +33,10 @@ def main():
           FT.append( int(config['ts3FileTransfer']['relayPort']) )
           FT.append( config['ts3FileTransfer']['remoteAddress'] )
           FT.append( int(config['ts3FileTransfer']['remotePort']) )
-          ts3Filetransfer = Tcp(FT[0],FT[1],FT[2])
+          ts3Filetransfer = Tcp(*FT[:3])
           t2 = threading.Thread(target=ts3Filetransfer.relay)
           t2.start()
-          print("FileTransfer: %s:%s <-> %s:%s"%(FT[0],FT[1],FT[2],FT[3]))
+          print("FileTransfer: %s:%s <-> %s:%s" % UDP)
 
         if config['ts3ServerQuery']['enabled']:
           SQ = []
@@ -44,10 +44,10 @@ def main():
           SQ.append( int(config['ts3ServerQuery']['relayPort']) )
           SQ.append( config['ts3ServerQuery']['remoteAddress'] )
           SQ.append( int(config['ts3ServerQuery']['remotePort']) )
-          ts3Serverquery = Tcp(SQ[0],SQ[1],SQ[2])
+          ts3Serverquery = Tcp(*SQ[:3])
           t3 = threading.Thread(target=ts3Serverquery.relay)
           t3.start()
-          print("ServerQuery: %s:%s <-> %s:%s"%(SQ[0],SQ[1],SQ[2],SQ[3]))
+          print("ServerQuery: %s:%s <-> %s:%s" % UDP)
 
     except KeyboardInterrupt:
         exit(0)
